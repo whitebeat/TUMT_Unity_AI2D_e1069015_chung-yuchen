bc404722a597d0c1224226469adb6b57f6e6e4c2
whitebeat <55227107+whitebeat@users.noreply.github.com>
2020-01-02 14:55:06 +0800

    介面

---
+++ Unity_UI_e1069015_ching-yuchen/Assets/scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;  //引用 音頻 api
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+   public AudioMixer mixer;
+
+   public Text textloading;
+
+   public Slider loading;
+
+
+   public void Setvbgm(float value)
+    {
+        mixer.SetFloat("vbgm", value);
+    }
+
+   public void Setvsfx(float value)
+    {
+        mixer.SetFloat("vsfx", value);
+    }
+
+   public void play()
+    {
+        //SceneManager.LoadScene("場經");
+        StartCoroutine(Loading());
+    }
+
+    private IEnumerator Loading()
+    {
+        //print("TEST 1");
+        //yield return new WaitForSeconds(1);
+        //print("TEST 2");
+
+        AsyncOperation ao = SceneManager.LoadSceneAsync("遊戲場景");
+        ao.allowSceneActivation = false;
+
+        while (ao.isDone == false)
+        {
+            textloading.text = ((ao.progress / 0.9f) * 100).ToString();
+            loading.value = ao.progress / 0.9f;
+            yield return new WaitForSeconds(0.0001f);
+
+            if (ao.progress == 0.9f)
+            {
+                ao.allowSceneActivation = true;
+            }
+        }
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene("介面");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Unity_UI_e1069015_ching-yuchen/Assets/scripts/player.cs
@@ -0,0 +1,126 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour
+{
+    [Header("血量與血條")]
+    public int hp = 100;
+    public Slider slider;
+
+    [Header("壽司區域")]
+    public Text textMaki;
+    public int MakiCount;
+    public int MakiTotal;
+    [Header("時間區域")]
+    public Text textTime;
+    public float gameTime;
+
+    [Header("結束畫布")]
+    public GameObject final;
+    public Text textBest;
+    public Text textCurrent;
+
+    public Text TextMaki
+    {
+        get
+        {
+            return textMaki;
+        }
+
+        set
+        {
+            textMaki = value;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.gameObject);
+
+        if (other.tag == "陷阱")
+        {
+            int d = other.GetComponent<trap>().damage;
+            hp -= d;
+            slider.value = hp;
+            if (hp <= 0) Dead();
+
+
+        }
+        if (other.tag == "Maki")
+        {
+            MakiCount++;
+            textMaki.text = "Maki:" + MakiCount + "/" + MakiTotal;
+            Destroy(other.gameObject);
+        }
+        if (other.name =="總點" && MakiCount == MakiTotal)
+        {
+            GameOver();
+        }
+    }
+
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.tag == "陷阱")
+        {
+            int d = other.GetComponent<trap>().damage;
+            hp -= d;
+            slider.value = hp;
+            if (hp <= 0) Dead();
+
+        }
+       
+       
+    }
+
+
+    private void Start()
+    {
+        if(PlayerPrefs.GetFloat("最佳紀錄") == 0)
+        {
+            PlayerPrefs.SetFloat("最佳紀錄", 999999);
+        }
+
+        MakiTotal = GameObject.FindGameObjectsWithTag("Maki").Length;
+        textMaki.text = "Maki:0/" + MakiTotal;
+    }
+
+    public void Update()
+    {
+        UpdateTime();
+    }
+
+    private void UpdateTime()
+    {
+        gameTime += Time.deltaTime;
+        textTime.text = gameTime.ToString("F2");
+    }
+
+    private void Dead()
+    {
+        final.SetActive(true);
+        textCurrent.text = "TIME : " + gameTime.ToString("F2");
+        textBest.text = "BEST" + PlayerPrefs.GetFloat("最佳紀錄").ToString("F2");
+        Cursor.lockState = CursorLockMode.None;
+
+        GetComponent<FPSControllerLPFP.FpsControllerLPFP>().enabled = false;
+        enabled = false;
+    }
+
+    private void GameOver()
+    {
+        final.SetActive(true);
+        textCurrent.text = "TIME : " + gameTime.ToString("F2");
+
+        if (gameTime < PlayerPrefs.GetFloat("最佳紀錄"))
+        {
+            PlayerPrefs.SetFloat("最佳紀錄", gameTime);
+        }
+        textBest.text = "BEST" + PlayerPrefs.GetFloat("最佳紀錄").ToString("F2");
+
+        Cursor.lockState = CursorLockMode.None;
+
+    }
+
+}
